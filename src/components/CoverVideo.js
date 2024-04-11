0e01936fbed2eac3a9544ba3bf10b6f37b0388f6
eduardovsousa <eduardo.varela@outlook.com.br>
2024-04-10 22:56:00 -0300

    feat: create navbar

--- src/components/CoverVideo.js
+++ src/components/CoverVideo.js
@@ -102,9 +102,9 @@ const CoverVideo = () => {
             r
           </motion.h1>
         </div>
-        <h2 data-scroll data-scroll-delay="0.04" data-scroll-speed="2">
+        <motion.h2 variants={item} data-scroll data-scroll-delay="0.04" data-scroll-speed="2">
           Inspire. Crie. Acredite
-        </h2>
+        </motion.h2>
       </Title>
       <video src={MainVideo} type="video/mp4" autoPlay muted loop />
     </VideoContainer>
